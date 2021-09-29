8ec00c608e5b85b535d829750160d923ca3848d9
MaxReeves <90580415+MaxReeves@users.noreply.github.com>
2021-09-29 10:24:08 +1000

    ajax cart update, actioncable catalog update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,25 +1,20 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
 
-  # GET /products or /products.json
   def index
-    @products = Product.all
+    @products = Product.all.order(:title)
   end
 
-  # GET /products/1 or /products/1.json
   def show
   end
 
-  # GET /products/new
   def new
     @product = Product.new
   end
 
-  # GET /products/1/edit
   def edit
   end
 
-  # POST /products or /products.json
   def create
     @product = Product.new(product_params)
 
@@ -35,12 +30,14 @@ class ProductsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /products/1 or /products/1.json
   def update
     respond_to do |format|
       if @product.update(product_params)
         format.html { redirect_to @product, notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
+
+        @products = Product.all.order(:title)
+        ActionCable.server.broadcast 'products', html: render_to_string('store/index', layout: false)
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
@@ -48,7 +45,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # DELETE /products/1 or /products/1.json
   def destroy
     @product.destroy
     respond_to do |format|
@@ -58,12 +54,10 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -20,8 +20,8 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
     end
 
-    assert_redirected_to line_item_url(LineItem.last)
-    assert_select 'li', "1 \u00D7 Programming Ruby 1.9"
+    assert_select 'h2', 'Your Cart'
+    assert_select 'td', "Programming Ruby 1.9"
   end
 
   test "should show line_item" do
@@ -47,4 +47,17 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to line_items_url
   end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      post line_items_url,
+      params: { product_id: products(:ruby).id },
+      xhr: true
+    end
+
+    assert_response :success
+    assert_match /<tr class=\\"line-item-highlight/,
+    @response.body
+  end
+
 end
