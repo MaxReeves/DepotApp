0a96f9f98534597dc5f4d64d6ed5dbe3517eb4c2
MaxReeves <90580415+MaxReeves@users.noreply.github.com>
2021-10-11 10:47:58 +1100

    add locales, user edgecase handling, tests and sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,22 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
-
   controller :sessions do
-    get 'login' => :new
+    get  'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
-
-  get 'admin/index'
-  get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-
-  root 'store#index', as: 'store_index'
-  resources :products
-
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
   end
 end
